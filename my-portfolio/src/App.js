ec6ea03b16dab21ab4ad6da248afbf6d8d02fc34
SuhasDhawale <suhasdhawale57@gmail.com>
2024-08-02 15:42:09 +0530

    imported all components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Dog from './components/Dog';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Dog />
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
     </div>
   );
 }
